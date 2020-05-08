18abeecb746d592e0840157330e276ed208c4c58
munjucho-corder <dailymunju@naver.com>
2020-05-08 17:59:45 +0900

    200508

--- recover_flower/js/main.js
+++ recover_flower/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready( function(){
 
+
+    //main_slide banner
     var mainswiper = new Swiper('.main-container', {
         slidesPerView: 1,
         spaceBetween: 30,
@@ -23,37 +25,48 @@ $(document).ready( function(){
         // },
     });
 
+    //menu_slide
     var mainMenuSlide = new Swiper('.main_menu-container', {
         slidesPerView: 10,
         breakpoints: {
 
-            1200: {
+            1400: {
                 slidesPerView: 10,
+                spaceBetween: 30,
+            },
+
+            1200: {
+                slidesPerView: 8,
                 spaceBetween: 30,
             },
             
             900: {
-                slidesPerView: 7,
+                slidesPerView: 6,
+                spaceBetween: 20,
+            },
+            800: {
+                slidesPerView: 5,
                 spaceBetween: 20,
             },
 
-            768: {
-                slidesPerView: 7,
+
+            700: {
+                slidesPerView: 5,
                 spaceBetween: 20,
             },
-
-            500: {
+            420: {
                 slidesPerView: 4,
                 spaceBetween: 20,
             },
             300: {
-                slidesPerView: 4,
+                slidesPerView: 3,
                 spaceBetween: 20,
             },
 
         },
     });
 
+    //tab_menu slide
     var subMenuSlide = new Swiper('.sub_menu-container', {
         slidesPerView: 6,
 
@@ -62,25 +75,46 @@ $(document).ready( function(){
             1200: {
                 slidesPerView: 6,
                 spaceBetween: 30,
-            },
- 
+            }, 
             768: {
                 slidesPerView: 4,
                 spaceBetween: 20,
             },
-
             500: {
                 slidesPerView: 3,
                 spaceBetween: 20,
             },
-
             300: {
                 slidesPerView: 3,
                 spaceBetween: 20,
-            },
-
-        },
+            }
+        }
     });
+
+    //ft_menu slide
+    var ftMenuSlide = new Swiper('.ft_menu-container', {
+        slidesPerView: 6,
+
+        breakpoints: {
+
+            1200: {
+                slidesPerView: 6,
+                spaceBetween: 30,
+            }, 
+            768: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+            500: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+            300: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            }
+        }
+    }); 
 
     $('.tablinks').each( function(){
         $(this).click( function(){            
